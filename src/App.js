f03f3ae62db2fd948036426b2f45dbb39568fd42
mehedihsajib <sajibmehedi7@gmail.com>
2021-10-07 00:56:51 +0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+    return (
+        <>
+            <div className="container">
+                <h1>React Bootstrap Sass Broilerplate</h1>
+            </div>
+        </>
+    );
+}
+
+export default App;
